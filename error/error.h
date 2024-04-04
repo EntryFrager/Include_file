63a316f840005fb2f58f38bf55dab826664b7dbb
EntryFrager <mpavlov375@gmail.com>
2024-04-04 14:05:03 +0300

    first commit

---
+++ error/error.h
@@ -0,0 +1,39 @@
+#ifndef ERROR_H
+#define ERROR_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define DEBUG
+
+#ifdef DEBUG
+    #define PRINT_ERROR()      if (code_error != ERR_NO) {my_strerr (code_error, stderr);}
+    #define ERR_RET(ret_value) if (code_error == NULL || *code_error != 0) {return ret_value;}
+#else
+    #define PRINT_ERROR()
+    #define ERR_RET(...)
+#endif
+
+#define my_assert(expr, cd_err) if (!(expr)) {  \
+        fprintf (stderr, "\x1b[31m%s %s:%d: My assertion failed: \"" #expr "\"\x1b[0m\n", __FILE__, __func__, __LINE__); \
+        *code_error |= cd_err;  \
+    }
+
+enum code_error {
+    ERR_NO            = 0,
+    ERR_PTR           = 1 << 0,
+    ERR_NAN           = 1 << 1,
+    ERR_MEM           = 1 << 2,
+    ERR_FOPEN         = 1 << 3,
+    ERR_FCLOSE        = 1 << 4,
+    ERR_FREAD         = 1 << 5,
+    ERR_DIV_ZERO      = 1 << 6,
+    ERR_SQRT_NEGAT    = 1 << 7,
+    ERR_LOG_NEGAT     = 1 << 8,
+
+    ERR_CNT           = 9
+};
+
+void my_strerr (unsigned code_error, FILE *stream);
+
+#endif // ERROR_H
---
+++ error/error_name.h
@@ -0,0 +1,22 @@
+#ifndef ERROR_NAME_H
+#define ERROR_NAME_H
+
+DEF_ERR ("ptr is null")
+
+DEF_ERR ("NaN value received")
+
+DEF_ERR ("memory allocation error")
+
+DEF_ERR ("an error occurred while opening the file")
+
+DEF_ERR ("an error occurred while closing the file")
+
+DEF_ERR ("an error occurred while reading text from the file")
+
+DEF_ERR ("division by zero")
+
+DEF_ERR ("root of a negative number")
+
+DEF_ERR ("the number under the logarithm is less than 0")
+
+#endif //ERROR_NAME_H
