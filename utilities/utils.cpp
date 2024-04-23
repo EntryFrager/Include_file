ec78361971e2f5453af324fb9dbc56f7cf17deec
EntryFrager <mpavlov375@gmail.com>
2024-04-23 17:28:56 +0300

    added a dynamic memory allocation tracking system

--- utilities/utils.cpp
+++ utilities/utils.cpp
@@ -3,6 +3,8 @@
 
 #include "utils.h"
 
+static const int COEF_REALLOC_UP = 2;
+
 char *get_file_to_str (FILE *stream, size_t *size_file, int *code_error)
 {
     my_assert(stream != NULL, ERR_PTR);
@@ -10,8 +12,7 @@ char *get_file_to_str (FILE *stream, size_t *size_file, int *code_error)
 
     *size_file = get_file_size(stream, code_error);
 
-    char *buf = (char *) calloc(*size_file + 1, sizeof (char));
-    my_assert(buf != NULL, ERR_MEM);
+    calloc_g_(buf, char *, *size_file + 1, sizeof(char));
 
     size_t read_size = fread(buf, sizeof (char), *size_file, stream);
     my_assert(read_size == *size_file, ERR_FREAD);
@@ -44,25 +45,22 @@ char *get_str (FILE *stream, int *code_error)
     size_t len = 0;
     size_t str_len = 10;
 
-    char *str = (char *) calloc(str_len, sizeof (char));
-    my_assert(str != NULL, ERR_MEM);
+    calloc_g_(str, char *, str_len, sizeof(char));
 
-    while (!isspace (ch = (char) fgetc(stream)))
+    while (!isspace(ch = (char)fgetc(stream)))
     {
         str[len++] = ch;
 
         if (len == str_len)
         {
-            str = (char *) realloc(str, str_len * 2);
-            my_assert(str != NULL, ERR_MEM);
+            realloc_(str, char *, str_len * COEF_REALLOC_UP);
 
-            str_len *= 2;
+            str_len *= COEF_REALLOC_UP;
         }
     }
 
     str[len++] = '\0';
-    str = (char *) realloc(str, len);
-    my_assert(str != NULL, ERR_MEM);
+    realloc_(str, char *, len);
 
     ERR_RET(NULL);
 
@@ -75,7 +73,7 @@ size_t get_n_lines (char *str, int *code_error)
 
     size_t n_lines = 0;
 
-    for (size_t i = 0; i < strlen(str); i++)
+    for (size_t i = 0; str[i] != '\0'; i++)
     {
         if (str[i] == '\n')
         {
@@ -90,8 +88,7 @@ char *skip_isspace (char *str, size_t len_str, int *code_error)
 {
     my_assert(str != NULL, ERR_PTR);
 
-    char *new_str = (char *) calloc(len_str, sizeof (char));
-    my_assert(new_str != NULL, ERR_MEM);
+    calloc_g_(new_str, char *, len_str, sizeof(char));
 
     size_t pos = 0;
 
@@ -103,8 +100,7 @@ char *skip_isspace (char *str, size_t len_str, int *code_error)
         }
     }
 
-    new_str = (char *) realloc(new_str, (pos + 1) * sizeof (char));
-    my_assert(new_str != NULL, ERR_MEM);
+    realloc_(new_str, char *, (pos + 1) * sizeof(char));
 
     new_str[pos] = '\0';
 
@@ -119,8 +115,7 @@ char *read_ident (char **str, int *code_error)
 {
     my_assert(str != NULL, ERR_PTR);
 
-    char *new_str = (char *) calloc(strlen (*str), sizeof (char));
-    my_assert(new_str != NULL, ERR_MEM);
+    calloc_g_(new_str, char *, strlen(*str), sizeof(char));
 
     size_t pos = 0;
 
@@ -130,8 +125,7 @@ char *read_ident (char **str, int *code_error)
         (*str)++;
     }
 
-    new_str = (char *) realloc(new_str, pos + 1);
-    my_assert(new_str != NULL, ERR_MEM);
+    realloc_(new_str, char *, pos + 1);
 
     new_str[pos] = '\0';
 
@@ -162,6 +156,52 @@ void clean_buffer ()
     int ch = 0;
 
     while (((ch = getchar()) != '\n') && (ch != EOF)) {}
+}
+
+void create_folder (const char *folder_name, int *code_error)
+{
+    my_assert(folder_name != NULL, ERR_PTR);
+
+    struct stat st = {0};
+
+    if (stat(folder_name, &st) == -1)
+    {
+        mkdir(folder_name, 0700);
+    }
+}
+
+static size_t n_ptr_dyn_mem = 0;
+static void **array_ptr_dyn_mem = NULL;
+
+void add_pointer_dyn_mem(void *ptr_dyn_mem, int *code_error)
+{
+    my_assert(ptr_dyn_mem != NULL, ERR_PTR);
+
+    if (array_ptr_dyn_mem == NULL)
+    {
+        calloc_init_(array_ptr_dyn_mem, void **, 5, sizeof(void *));
+    }
+    else if (n_ptr_dyn_mem == sizeof(array_ptr_dyn_mem))
+    {
+        realloc_(array_ptr_dyn_mem, void **, n_ptr_dyn_mem * COEF_REALLOC_UP);
+        n_ptr_dyn_mem *= COEF_REALLOC_UP;
+    }
+
+    array_ptr_dyn_mem[n_ptr_dyn_mem] = ptr_dyn_mem;
+}
+
+void free_ptr_dyn_mem ()
+{
+    for (size_t i = 0; i < n_ptr_dyn_mem; i++)
+    {
+        free(array_ptr_dyn_mem[i]);
+        array_ptr_dyn_mem[i] = NULL;
+    }
+
+    free(array_ptr_dyn_mem);
+    array_ptr_dyn_mem = NULL;
+
+    n_ptr_dyn_mem = 0;
 }
 
 #endif // UTILS_CPP
--- utilities/utils.h
+++ utilities/utils.h
@@ -6,17 +6,54 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <sys/stat.h>
 
-#include "error.h"
+#include "../error/error.h"
 
 #define compare(first_value, second_value) compare_number(first_value, second_value, code_error)
 
-#define FOPEN_(fp, fp_name, mode)               \
+#define fopen_(fp, fp_name, mode)               \
     FILE *fp = fopen(fp_name, mode);            \
     my_assert(fp != NULL, ERR_FOPEN);
 
-#define FCLOSE_(fp)                             \
-    my_assert(fclose (fp) == 0, ERR_FCLOSE);
+#define fclose_(fp)                             \
+    my_assert(fclose(fp) == 0, ERR_FCLOSE);
+
+#define calloc_(var_name, type, size, size_elem)            \
+    type var_name = (type) calloc(size, size_elem);         \
+    my_assert(var_name != NULL, ERR_MEM);
+
+#define calloc_init_(var_name, type, size, size_elem)       \
+    var_name = (type) calloc(size, size_elem);              \
+    my_assert(var_name != NULL, ERR_MEM);
+
+#define calloc_g_(var_name, type, size, size_elem)          \
+    calloc_(var_name, type, size, size_elem);               \
+    add_pointer_dyn_mem((void *) var_name, code_error);
+
+#define calloc_init__g_(var_name, type, size, size_elem)    \
+    calloc_init_(var_name, type, size, size_elem);          \
+    add_pointer_dyn_mem((void *) var_name, code_error);
+
+#define malloc_(var_name, type, size)                       \
+    type var_name = (type) malloc(size);                    \
+    my_assert(var_name != NULL, ERR_MEM);
+
+#define malloc_init_(var_name, type, size)                  \
+    var_name = (type) malloc(size);                         \
+    my_assert(var_name != NULL, ERR_MEM);
+
+#define malloc_g_(var_name, type, size, size_elem)          \
+    malloc_(var_name, type, size, size_elem);               \
+    add_pointer_dyn_mem((void *) var_name, code_error);
+
+#define malloc_init_g_(var_name, type, size, size_elem)     \
+    malloc_init_(var_name, type, size, size_elem);          \
+    add_pointer_dyn_mem((void *) var_name, code_error);
+
+#define realloc_(ptr, type, new_size)                       \
+    ptr = (type) realloc(ptr, new_size);                    \
+    my_assert(ptr != NULL, ERR_MEM);
 
 const double EPSILON = 1e-10;
 
@@ -35,5 +72,11 @@ char *read_ident (char **str, int *code_error);
 int compare_number (const double value_1, const double value_2, int *code_error);
 
 void clean_buffer ();
+
+void create_folder (const char *folder_name, int *code_error);
+
+void add_pointer_dyn_mem (void *ptr_dyn_mem, int *code_error);
+
+void free_ptr_dyn_mem ();
 
 #endif //UTILS_H
